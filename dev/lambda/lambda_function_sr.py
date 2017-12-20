2d3cc67334c198190501ac6afbdb0948171f3700
sdrohrer <stephen.rohrer86@gmail.com>
2017-12-20 13:43:52 -0500

    Update lambda_function_sr.py

--- dev/lambda/lambda_function_sr.py
+++ dev/lambda/lambda_function_sr.py
@@ -5,16 +5,24 @@
 #This tutorial https://www.youtube.com/watch?v=hzlxWBs1Qt4 covers it (just first 8 minutes)
 ########################################
 import json
+# you import the json package to use this
 
 print('Loading function')
 
-
+#lambda takes a few inputs, so far I only understand event as the trigger
+#not sure about context or callback which is not mentioned yet
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
     
-
+    #ignore this print command for now
+    ##print("Received event: " + json.dumps(event, indent=2))
+    
+    #these print commands only show up in the log
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
+    
+    #this return command is ultimately the only thing rendered. meaning only the key1 passed to the class is output
     return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    
+    #ignore this raise command for now
+    ##raise Exception('Something went wrong')
